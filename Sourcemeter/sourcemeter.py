a0384c32eefbaa4c3d5bf174096da3da31f10b00
cnmuser <cnmuser@CSI367794.anl.gov>
2026-04-22 18:39:12 -0500

    feat: noise

--- Sourcemeter/sourcemeter.py
+++ Sourcemeter/sourcemeter.py
@@ -135,7 +135,7 @@ if __name__ == "__main__":
         print(len(data["voltage"]))
         sm.save_data_csv(
             data,
-            f"./Sourcemeter/sourcemeter_data/sm_data_{speed_nplc}_{duration}s_{dt}s_{formatted_time}_AFM1_2.csv",
+            f"./Sourcemeter/sourcemeter_data/sm_data_{speed_nplc}_{duration}s_{dt}s_{formatted_time}_50Ohm.csv",
         )
     except Exception as e:
         print(f"An error occurred: {e}")
