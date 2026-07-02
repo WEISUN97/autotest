1933c46aa0881b3a528e63392253d5f238b7739f
cnmuser <cnmuser@CSI367794.anl.gov>
2026-07-02 16:06:05 -0500

    feat: auto update 202607021606 FIB2 update

--- main_BCP303_interval_measure.py
+++ main_BCP303_interval_measure.py
@@ -127,9 +127,9 @@ if __name__ == "__main__":
     # Define default values for the stage movement
     return_back = False  # Set to True if you want the stage to return to the starting position after each repeat
     setting_test = {
-        "start_position": 0.5,
+        "start_position": 0,
         "step_size": 0.05,
-        "step_number": 25,
+        "step_number": 20,
         "step_size_z": 0,
         "repeat_number": 1,
         "position_z": 0,
@@ -152,7 +152,7 @@ if __name__ == "__main__":
         chip_name="V1_R_W_1_Left",
         # chip_name="SiN_beam"
         # sample_name="AFM3_450_boundary_2",
-        sample_name=f"test_AFM3_450_w20_4{'_return' if return_back else ''}",
+        sample_name=f"test_AFM3_450_w15_4{'_return' if return_back else ''}",
         # sample_name="w2",  # test_1_right, w=20
         ifshow=False,  # if show F-X curve
         show_signal=False,  # if show voltage signal (if show F-X, show_signal will be set to False automatically)
