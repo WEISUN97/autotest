4f17f0a32620a8b43ea90c07d4e79215843d0453
cnmuser <cnmuser@CSI367794.anl.gov>
2026-07-04 11:47:08 -0500

    feat: auto update 202607041147 folder structure update

--- main_BCP303_interval_measure.py
+++ main_BCP303_interval_measure.py
@@ -129,8 +129,8 @@ if __name__ == "__main__":
     return_back = False  # Set to True if you want the stage to return to the starting position after each repeat
     setting_test = {
         "start_position": 0.0,
-        "step_size": 0.02,
-        "step_number": 100,
+        "step_size": 0.1,
+        "step_number": 20,
         "step_size_z": 0,
         "repeat_number": 1,
         "position_z": 0,
