6c7844bfbd470cd26d95018e7d71e6def44a7bf4
cnmuser <cnmuser@CSI367794.anl.gov>
2026-07-04 11:47:03 -0500

    feat: auto update 202607041147 folder structure update

--- main_BCP303_interval_measure.py
+++ main_BCP303_interval_measure.py
@@ -144,7 +144,7 @@ if __name__ == "__main__":
         chip_name="V1_R_W_1_Left",
         # chip_name="SiN_beam"
         # sample_name="AFM3_450_boundary_2",
-        sample_name=f"test_AFM3_450_w10_4{'_return' if return_back else ''}",
+        sample_name=f"test_AFM3_450_w20_4{'_return' if return_back else ''}",
         # sample_name="w2",  # test_1_right, w=20
         ifshow=False,  # if show F-X curve
         show_signal=False,  # if show voltage signal (if show F-X, show_signal will be set to False automatically)
