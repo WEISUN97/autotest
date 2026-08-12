43b562f5299943b9383fd8f19ffffccfbb809808
Wei Sun <wsun7@binghamton.edu>
2026-08-12 16:38:38 -0400

    feat: auto update 202608121638 afm6 test

--- main_BCP303_interval_measure.py
+++ main_BCP303_interval_measure.py
@@ -131,9 +131,9 @@ if __name__ == "__main__":
     # Define default values for the stage movement
     return_back = False  # Set to True if you want the stage to return to the starting position after each repeat
     setting_test = {
-        "start_position": 1,
-        "step_size": 0.02,
-        "step_number": 150,
+        "start_position": 0.5,
+        "step_size": 0.1,
+        "step_number": 30,
         "step_size_z": 0,
         "repeat_number": 1,
         "position_z": 0,
@@ -147,7 +147,7 @@ if __name__ == "__main__":
         # chip_name="V1_R_S_1_L",
         # chip_name="SiN_beam"
         # sample_name="AFM3_450_boundary_2"
-        sample_name=f"AFM6_450_test_w20_2{'_return' if return_back else ''}",
+        sample_name=f"AFM6_450_test_w20_4{'_return' if return_back else ''}",
         # sample_name="w2",  # test_1_right, w=20
         ifshow=False,  # if show F-X curve
         show_signal=False,  # if show voltage signal (if show F-X, show signal will be set to False automatically)
